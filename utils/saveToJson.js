18859e8ba3e5ebe360b533f8d1884b8339cd93de
Jovid1242 <jovid1242@gmail.com>
2024-05-21 14:36:24 +0400

    fix

--- utils/saveToJson.js
+++ utils/saveToJson.js
@@ -2,24 +2,21 @@ const fs = require("fs");
 const path = require("path");
 var filePath = path.join(__dirname, "../db/user.json");
 
-// Функция для сохранения данных в JSON файл
 function saveDataToJson(data) {
   try {
-    const jsonData = JSON.stringify(data, null, 2); // Преобразуем данные в JSON формат с отступами
-    fs.writeFileSync(filePath, jsonData); // Записываем данные в файл
-    console.log(`Данные успешно сохранены в ${filePath}`);
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFileSync(filePath, jsonData);
   } catch (err) {
     console.error(`Ошибка сохранения данных: ${err}`);
   }
 }
 
-// Функция для чтения данных из JSON файла
 function readDataFromJson(filePath) {
   try {
-    const jsonData = fs.readFileSync(filePath, 'utf8');
+    const jsonData = fs.readFileSync(filePath, "utf8");
     const data = JSON.parse(jsonData);
     if (!Array.isArray(data)) {
-      throw new Error('Данные не являются массивом');
+      throw new Error("Данные не являются массивом");
     }
     return data;
   } catch (err) {
@@ -30,20 +27,16 @@ function readDataFromJson(filePath) {
 
 function updateDataInJson(chatId, updateData) {
   try {
-    const data = readDataFromJson(filePath); // Читаем существующие данные
+    const data = readDataFromJson(filePath);
     if (data) {
       const record = data.find((item) => item.chatId === chatId);
       if (record) {
-        // Обновляем вложенные данные
         if (record.detail) {
           Object.assign(record.detail, updateData);
         } else {
           record.detail = updateData;
         }
-        saveDataToJson(data); // Сохраняем обновленные данные
-        console.log(
-          `Данные успешно обновлены для chatId ${chatId} в ${filePath}`
-        );
+        saveDataToJson(data);
       } else {
         console.error(
           `Ошибка обновления данных: chatId ${chatId} не найден в ${filePath}`
@@ -61,51 +54,35 @@ function updateDataInJson(chatId, updateData) {
 
 function addDataToJson(newData) {
   try {
-    let data = readDataFromJson(filePath); // Читаем существующие данные
-    data.push(newData); // Добавляем новые данные в массив
-    saveDataToJson(data); // Сохраняем обновленные данные
-    console.log(`Данные успешно добавлены в ${filePath}`);
+    let data = readDataFromJson(filePath);
+    data.push(newData);
+    saveDataToJson(data);
   } catch (err) {
     console.error(`Ошибка добавления данных: ${err}`);
   }
 }
 
-// Функция для проверки наличия данных по chatId
 function checkChatIdExists(chatId) {
   try {
-    const data = readDataFromJson(filePath); // Читаем данные из файла
-    const found = data.some((item) => item.chatId === chatId); // Проверяем наличие chatId в массиве
+    const data = readDataFromJson(filePath);
+    const found = data.some((item) => item.chatId === chatId);
     return found;
   } catch (err) {
     console.error(`Ошибка проверки chatId: ${err}`);
-    return false; // Возвращаем false, если возникла ошибка
+    return false;
   }
 }
 
-// Функция для поиска данных по chatId
 function findDataByChatId(chatId) {
   try {
-    const data = readDataFromJson(filePath); // Читаем данные из файла
-    const foundItem = data.find((item) => item.chatId === chatId); // Находим объект с заданным chatId
-    return foundItem; // Возвращаем найденный объект или undefined, если объект не найден
+    const data = readDataFromJson(filePath);
+    const foundItem = data.find((item) => item.chatId === chatId);
+    return foundItem;
   } catch (err) {
     console.error(`Ошибка поиска данных: ${err}`);
-    return undefined; // Возвращаем undefined, если возникла ошибка
+    return undefined;
   }
 }
-
-// Примеры использования:
-
-// Сохранение данных
-// const myData = { name: "John", age: 30 };
-// saveDataToJson(dataFile, myData);
-
-// Чтение данных
-// const readData = readDataFromJson(dataFile);
-// console.log(readData); // Вывод: { name: "John", age: 30 }
-
-// // Обновление данных
-// updateDataInJson(dataFile, { age: 35 });
 
 module.exports = {
   readDataFromJson,
